455ff5a11a3fb4850e873a1c85c0a77cd004302a
“mgiraldoh” <maugirhur@hotmail.com>
2021-07-05 06:54:17 -0400

    AHora Si con user de guthub

---
+++ src/clase2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class clase2 {
+    public static void main(String[] args){
+
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("dame tu nombre");
+        String nombre=scanner.nextLine();
+        System.out.println("Qui_hay "+nombre);
+    }
+    
+} 
